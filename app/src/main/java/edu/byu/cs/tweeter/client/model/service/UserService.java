ddf3877554de10c722b6acfaa130662d7de5e70f
Ben Kinard <benjaminjameskinard@gmail.com>
2022-01-25 15:57:22 -0700

    Optimize Imports for GetUserTask and FollowingFragment

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -1,19 +1,14 @@
 package edu.byu.cs.tweeter.client.model.service;
 
-import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetUserTask;
-import edu.byu.cs.tweeter.client.presenter.FollowingPresenter;
-import edu.byu.cs.tweeter.client.view.main.MainActivity;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
