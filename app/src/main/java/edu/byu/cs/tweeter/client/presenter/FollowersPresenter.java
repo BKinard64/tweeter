21b32ed71e7de7540b0d2bc67d79fae523eda76b
Ben Kinard <benjaminjameskinard@gmail.com>
2022-01-26 14:35:54 -0700

    Optimize Imports following FollowersFragment Refactor

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/FollowersPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FollowersPresenter.java
@@ -1,13 +1,10 @@
 package edu.byu.cs.tweeter.client.presenter;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
-import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowersTask;
 import edu.byu.cs.tweeter.model.domain.User;
 
 public class FollowersPresenter {
