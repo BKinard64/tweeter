b3b19614990f6888b2eeae93e609ca2f7f37a654
Ben Kinard <benjaminjameskinard@gmail.com>
2022-01-26 18:49:58 -0700

    Refactor GetUserTask from FeedFragment

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.StatusService;
+import edu.byu.cs.tweeter.client.model.service.UserService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFeedTask;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
@@ -18,10 +19,12 @@ public class FeedPresenter {
         void displayMessage(String message);
         void setLoadingStatus(boolean value);
         void addStatuses(List<Status> statuses);
+        void goToUserPage(User user);
     }
 
     private View view;
     private StatusService statusService;
+    private UserService userService;
 
     private Status lastStatus;
     private boolean hasMorePages;
@@ -30,6 +33,7 @@ public class FeedPresenter {
     public FeedPresenter(View view) {
         this.view = view;
         statusService = new StatusService();
+        userService = new UserService();
     }
 
     public boolean hasMorePages() {
@@ -58,6 +62,12 @@ public class FeedPresenter {
         }
     }
 
+    public void onUserClick(String userAlias) {
+        userService.getUser(Cache.getInstance().getCurrUserAuthToken(), userAlias,
+                            new GetUserObserver());
+        view.displayMessage("Getting user's profile...");
+    }
+
     public class GetFeedObserver implements StatusService.GetFeedObserver {
         @Override
         public void handleSuccess(List<Status> statuses, boolean hasMorePages) {
@@ -83,6 +93,23 @@ public class FeedPresenter {
             view.setLoadingStatus(false);
 
             view.displayMessage("Failed to get feed because of exception: " + ex.getMessage());
+        }
+    }
+
+    public class GetUserObserver implements UserService.GetUserObserver {
+        @Override
+        public void handleSuccess(User user) {
+            view.goToUserPage(user);
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            view.displayMessage("Failed to get user's profile: " + message);
+        }
+
+        @Override
+        public void handleException(Exception ex) {
+            view.displayMessage("Failed to get user's profile because of exception: " + ex.getMessage());
         }
     }
 }
