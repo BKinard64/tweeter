165137b2c7991b64be56bc35789ae36444870566
Ben Kinard <benjaminjameskinard@gmail.com>
2022-01-26 18:50:47 -0700

    Optimize Imports from Refactor of FeedFragment

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
@@ -1,13 +1,10 @@
 package edu.byu.cs.tweeter.client.presenter;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.StatusService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
-import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFeedTask;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
