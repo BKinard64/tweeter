50c7c8bab8356d72eec269d58994359cb86d79ff
Ben Kinard <benjaminjameskinard@gmail.com>
2022-01-28 12:16:34 -0700

    Refactor PostStatusTask from MainActivity

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -2,6 +2,7 @@ package edu.byu.cs.tweeter.client.model.service;
 
 import android.os.Handler;
 import android.os.Message;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -9,8 +10,12 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFeedTask;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetStoryTask;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.PostStatusTask;
+import edu.byu.cs.tweeter.client.presenter.MainPresenter;
+import edu.byu.cs.tweeter.client.view.main.MainActivity;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
@@ -92,6 +97,43 @@ public class StatusService {
                 observer.handleFailure(message);
             } else if (msg.getData().containsKey(GetFeedTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(GetFeedTask.EXCEPTION_KEY);
+                observer.handleException(ex);
+            }
+        }
+    }
+
+    public interface PostStatusObserver {
+        void handleSuccess();
+        void handleFailure(String message);
+        void handleException(Exception ex);
+    }
+
+    public void postStatus(AuthToken currUserAuthToken, Status newStatus, PostStatusObserver postStatusObserver) {
+        PostStatusTask statusTask = new PostStatusTask(currUserAuthToken, newStatus,
+                                                        new PostStatusHandler(postStatusObserver));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(statusTask);
+    }
+
+    // PostStatusHandler
+
+    private class PostStatusHandler extends Handler {
+        private PostStatusObserver observer;
+
+        public PostStatusHandler(PostStatusObserver observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(PostStatusTask.SUCCESS_KEY);
+            if (success) {
+                observer.handleSuccess();
+            } else if (msg.getData().containsKey(PostStatusTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(PostStatusTask.MESSAGE_KEY);
+                observer.handleFailure(message);
+            } else if (msg.getData().containsKey(PostStatusTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(PostStatusTask.EXCEPTION_KEY);
                 observer.handleException(ex);
             }
         }
--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/MainPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/MainPresenter.java
@@ -1,5 +1,12 @@
 package edu.byu.cs.tweeter.client.presenter;
 
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,6 +17,8 @@ import edu.byu.cs.tweeter.client.model.service.StatusService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.IsFollowerTask;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.LogoutTask;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.PostStatusTask;
+import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
 public class MainPresenter {
@@ -19,6 +28,7 @@ public class MainPresenter {
         void setCount(boolean isFollowingCount, int count);
         void updateFollowButton(boolean removed);
         void enableFollowButton(boolean value);
+        void successfulPost();
         void logoutUser();
     }
 
@@ -62,6 +72,77 @@ public class MainPresenter {
 
     public void onLogoutButtonClicked() {
         userService.logout(Cache.getInstance().getCurrUserAuthToken(), new LogoutObserver());
+    }
+
+    public void onStatusPosted(String post) throws ParseException, MalformedURLException {
+        Status newStatus = new Status(post, Cache.getInstance().getCurrUser(), getFormattedDateTime(),
+                                        parseURLs(post), parseMentions(post));
+        statusService.postStatus(Cache.getInstance().getCurrUserAuthToken(), newStatus,
+                                new PostStatusObserver());
+    }
+
+    private String getFormattedDateTime() throws ParseException {
+        SimpleDateFormat userFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat statusFormat = new SimpleDateFormat("MMM d yyyy h:mm aaa");
+
+        return statusFormat.format(userFormat.parse(LocalDate.now().toString() + " " + LocalTime.now().toString().substring(0, 8)));
+    }
+
+    private List<String> parseURLs(String post) throws MalformedURLException {
+        List<String> containedUrls = new ArrayList<>();
+        for (String word : post.split("\\s")) {
+            if (word.startsWith("http://") || word.startsWith("https://")) {
+
+                int index = findUrlEndIndex(word);
+
+                word = word.substring(0, index);
+
+                containedUrls.add(word);
+            }
+        }
+
+        return containedUrls;
+    }
+
+    private List<String> parseMentions(String post) {
+        List<String> containedMentions = new ArrayList<>();
+
+        for (String word : post.split("\\s")) {
+            if (word.startsWith("@")) {
+                word = word.replaceAll("[^a-zA-Z0-9]", "");
+                word = "@".concat(word);
+
+                containedMentions.add(word);
+            }
+        }
+
+        return containedMentions;
+    }
+
+    private int findUrlEndIndex(String word) {
+        if (word.contains(".com")) {
+            int index = word.indexOf(".com");
+            index += 4;
+            return index;
+        } else if (word.contains(".org")) {
+            int index = word.indexOf(".org");
+            index += 4;
+            return index;
+        } else if (word.contains(".edu")) {
+            int index = word.indexOf(".edu");
+            index += 4;
+            return index;
+        } else if (word.contains(".net")) {
+            int index = word.indexOf(".net");
+            index += 4;
+            return index;
+        } else if (word.contains(".mil")) {
+            int index = word.indexOf(".mil");
+            index += 4;
+            return index;
+        } else {
+            return word.length();
+        }
     }
 
     public class GetFollowersCountObserver implements FollowService.GetFollowersCountObserver {
@@ -183,6 +264,23 @@ public class MainPresenter {
         @Override
         public void handleException(Exception ex) {
             view.displayMessage("Failed to logout because of exception: " + ex.getMessage());
+        }
+    }
+
+    public class PostStatusObserver implements StatusService.PostStatusObserver {
+        @Override
+        public void handleSuccess() {
+            view.successfulPost();
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            view.displayMessage("Failed to post status: " + message);
+        }
+
+        @Override
+        public void handleException(Exception ex) {
+            view.displayMessage("Failed to post status because of exception: " + ex.getMessage());
         }
     }
 }
