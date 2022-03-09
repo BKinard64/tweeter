d1d9bb8c13c066b21c5ccef0dfe85cd19a650747
Ben Kinard <benjaminjameskinard@gmail.com>
2022-03-09 14:31:00 -0700

    Have RegisterRequest extend LoginRequest

--- shared/src/main/java/edu/byu/cs/tweeter/model/net/request/LoginRequest.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/net/request/LoginRequest.java
@@ -4,7 +4,7 @@ public class LoginRequest {
     private String username;
     private String password;
 
-    private LoginRequest() {}
+    public LoginRequest() {}
 
     public LoginRequest(String username, String password) {
         this.username = username;
